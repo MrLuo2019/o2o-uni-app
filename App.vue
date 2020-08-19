648df770d0929da233e1cda65000269d44fae0be
罗文杰 <wenjie_luo@hydf.cn>
2020-08-19 12:21:19 +0800

    first commit

---
+++ App.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+	onLaunch: function() {
+		console.log('App Launch');
+	},
+	onShow: function() {
+		console.log('App Show');
+	},
+	onHide: function() {
+		console.log('App Hide');
+	}
+};
+</script>
+
+<style>
+/* #ifndef APP-PLUS-NVUE */
+/* uni.css - 通用组件、模板样式库，可以当作一套ui库应用 */
+@import './common/uni.css';
+/* #endif*/
+
+/* 解决头条小程序组件内引入字体不生效的问题 */
+/* #ifdef MP-TOUTIAO */
+@font-face {
+	font-family: uniicons;
+	src: url('/static/uni.ttf');
+}
+/* #endif */
+
+page {
+	background-color: #F5F5F5;
+	height: 100%;
+	font-size: 28rpx;
+	line-height: 1.8;
+}
+</style>
